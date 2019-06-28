420711370dd237771fdb7a2c989210855c2cf62b
Cedxx <cedmsm@hotmail.com>
2019-06-28 11:13:15 +0200

    created member variable for image and buttons in MainActivity.java Linked element from layout for image and buttons

--- app/src/main/java/cedric/druesnes/moodtracker/MainActivity.java
+++ app/src/main/java/cedric/druesnes/moodtracker/MainActivity.java
@@ -2,12 +2,26 @@ package cedric.druesnes.moodtracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Member variable
+    ImageView mMoodImage;
+    ImageButton mCommentImage;
+    ImageButton mHistoryButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    //Linked the elements in the layout to Java code
+        mMoodImage = findViewById(R.id.moodView);
+        mCommentImage = findViewById(R.id.commentButton);
+        mHistoryButton = findViewById(R.id.historyButton);
+
     }
 }
