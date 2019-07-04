0a38e86479f63f33fd084216d01e4569f6c9b549
Cedxx <cedmsm@hotmail.com>
2019-07-04 14:49:38 +0200

    remove gesture code from main activity Created new Class MyGestureLister and added the onFling variable

--- app/src/main/java/cedric/druesnes/moodtracker/MainActivity.java
+++ app/src/main/java/cedric/druesnes/moodtracker/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,46 +35,7 @@ public class MainActivity extends AppCompatActivity {
                 R.drawable.smiley_normal,
                 R.drawable.smiley_disappointed,
                 R.drawable.smiley_sad};
-
-        //this is the view we will add the gesture detector to
-        View myView = findViewById(R.id.moodView);
-
-        // get the gesture detector
-        mDetector = new GestureDetector(this, new MyGestureListener());
-
-        //add a touch listener to the view
-        // the touch lister passes all its events on to the gesture detector
-        myView.setOnTouchListener(touchListener);
     }
 
-    // This touch listener passes everything on to the gesture detector.
-    // That saves us the trouble of interpreting the raw touch events
-    // ourselves.
-    View.OnTouchListener touchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View v, MotionEvent event) {
-            // pass the events to the gesture detector
-            // a return value of true means the detector is handling it
-            // a return value of false means the detector didn't
-            // recognize the event
-            return mDetector.onTouchEvent(event);
-        }
-    };
-
-
-    // In the SimpleOnGestureListener subclass you should override onDown and any other gesture that you want to detect.
-    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
-
-        @Override
-        public boolean onDown(MotionEvent event) { // MUST return true here or else none of the other gestures will work
-            return true;
-        }
-
-        @Override
-        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-            Log.d("TAG", "onFling: ");
-            return true;
-        }
-    }
 }
 
