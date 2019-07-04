cd7a76424c68fcfc774d5ec00efc6d314cfbc504
Cedxx <cedmsm@hotmail.com>
2019-07-04 23:57:46 +0200

    tested switch case with 2 images Code is working

--- app/src/main/java/cedric/druesnes/moodtracker/MainActivity.java
+++ app/src/main/java/cedric/druesnes/moodtracker/MainActivity.java
@@ -2,10 +2,8 @@ package cedric.druesnes.moodtracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
-import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -21,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     public ImageView mMoodImage;
     public ImageButton mCommentImage;
     public ImageButton mHistoryButton;
+    private int mCurrentMood = 3;
 
 
 
@@ -35,6 +34,26 @@ public class MainActivity extends AppCompatActivity {
         mMoodImage = findViewById(R.id.moodView);
         mCommentImage = findViewById(R.id.commentButton);
         mHistoryButton = findViewById(R.id.historyButton);
+
+    }
+
+    //Mood Image variable
+    private int changeMood (int currentMood){
+        if (currentMood < 0){
+            currentMood = 0;
+        }else if (currentMood > 4){
+            currentMood = 4;
+        }
+        mCurrentMood = currentMood;
+        switch (currentMood) {
+            case 0:
+                mMoodImage.setImageResource(R.drawable.smiley_normal);
+            break;
+            case 1:
+                mMoodImage.setImageResource(R.drawable.smiley_sad);
+            break;
+        }
+        return currentMood;
 
     }
 
@@ -69,9 +88,11 @@ public class MainActivity extends AppCompatActivity {
                     // left to right swipe
                     Toast.makeText(getApplicationContext(), "Swipe left to right", Toast.LENGTH_SHORT).show();
                 } else if (e1.getY() - e2.getY() > SWIPE_MIN_DISTANCE && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY) {
+                    changeMood(--mCurrentMood);
                     // bottom to top
                     Toast.makeText(getApplicationContext(), "Swipe bottom to top", Toast.LENGTH_SHORT).show();
                 } else if (e2.getY() - e1.getY() > SWIPE_MIN_DISTANCE && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY) {
+                    changeMood(++mCurrentMood);
                     // top to bottom
                     Toast.makeText(getApplicationContext(), "Swipe top to bottom", Toast.LENGTH_SHORT).show();
                 }
