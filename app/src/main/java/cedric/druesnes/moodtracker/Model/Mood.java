6afdb84fd1110e07b4a3578538d4d5c92a91f34e
Cedxx <cedmsm@hotmail.com>
2019-09-27 22:15:40 +0200

    Added a new Mood.java class in the model for the SQL database

---
+++ app/src/main/java/cedric/druesnes/moodtracker/Model/Mood.java
@@ -0,0 +1,29 @@
+package cedric.druesnes.moodtracker.Model;
+
+import android.provider.BaseColumns;
+
+public final class Mood {
+    // To prevent someone from accidentally instantiating the contract class,
+    // make the constructor private.
+    private Mood() {}
+
+    //create the database entries for each field that we want to save
+    public static class MoodEntry implements BaseColumns{
+        public static final String TABLE_NAME = "moodDB";
+        public static final String COLUMN_COMMENT = "Comment";
+        public static final String COLUMN_MOOD_INDEX = "Mood";
+        public static final String COLUMN_DATE = "Date";
+    }
+
+    //Create the DATABASE
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE" + MoodEntry.TABLE_NAME + " (" +
+            MoodEntry._ID + " INTEGER PRIMARY KEY," +
+            MoodEntry.COLUMN_COMMENT + " TEXT," +
+            MoodEntry.COLUMN_MOOD_INDEX + " INT," +
+            MoodEntry.COLUMN_DATE + "TEXT)";
+
+    //Drop the table is it already exists
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + MoodEntry.TABLE_NAME;
+}
